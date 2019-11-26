346f60c5c485d1e1a8e302d2f3b5b1b1de1d5e24
Gaetan Puleo <gaetan.puleo.dev@gmail.com>
2019-11-26 09:06:57 +0100

    feat(helmet): add helmet to protect microservice

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from 'mongoose';
 import './initModels';
 import bodyparser from 'body-parser';
+import helmet from 'helmet';
 import router from './router';
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use('/', router);
